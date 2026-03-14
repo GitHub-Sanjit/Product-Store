c5adc7bacd43d80e06f3f347f11ed5453b3a3402
sanjit <sanjit.est14@gmail.com>
2026-03-15 01:07:24 +0600

    Building our API

--- backend/server.js
+++ backend/server.js
@@ -2,16 +2,20 @@ import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
 
-dotenv.config();
+import productRoute from "./routes/product.route.js";
+
+dotenv.config({
+  // path: "./.env",
+});
 
 const app = express();
 
-app.get("/products", (req, res) => {});
+app.use(express.json());
+
+app.use("/api/products", productRoute);
 // connectDB();
 
 app.listen(5000, () => {
   connectDB();
   console.log("Server is running on http://localhost:5000");
 });
-
-//
